40b33a181ba3bccf4219dbd945f5649fa62f1f66
jaeyounglee <ljyda214@gmail.com>
2025-06-03 18:33:04 +0900

    test: 사칙연산 메서드들에 대한 테스트 코드 추가

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,19 +1,69 @@
 package com.group.libraryapp.calculator
 
-import org.assertj.core.api.Assertions
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CalculatorTest {
+    private lateinit var calculator: Calculator
+
+    companion object {
+        private const val INIT_VALUE = 5
+        private const val OPERAND_VALUE = 3
+    }
+
+    @BeforeEach
+    fun setUp() {
+        calculator = Calculator(INIT_VALUE)
+    }
 
     @Test
     fun addTest() {
-        val calculator = Calculator(5)
-        calculator.add(3)
+        calculator.add(OPERAND_VALUE)
 
-        assertThat(calculator).isEqualTo(Calculator(8))
+        assertThat(calculator).isEqualTo(Calculator(INIT_VALUE + OPERAND_VALUE))
+    }
 
-//        val calculatorV2 = CalculatorV2(5)
-//        assertThat(calculatorV2.number).isEqualTo(5)
+    @Test
+    fun minusTest() {
+        calculator.minus(OPERAND_VALUE)
+
+        assertThat(calculator).isEqualTo(Calculator(INIT_VALUE - OPERAND_VALUE))
+    }
+
+    @Test
+    fun multiplyTest() {
+        calculator.multiply(OPERAND_VALUE)
+
+        assertThat(calculator).isEqualTo(Calculator(INIT_VALUE * OPERAND_VALUE))
+    }
+
+    @Nested
+    @DisplayName("계신기에서 나눗셈을 할 때")
+    inner class WhenDivide {
+        @ParameterizedTest
+        @ValueSource(ints = [1, 2, 3])
+        fun do_divide(input: Int) {
+            calculator.divide(input)
+
+            assertThat(calculator).isEqualTo(Calculator(INIT_VALUE / input))
+        }
+
+        @DisplayName("나누고자 하는 분모가 0 일 때, 예외를 던진다.")
+        @ParameterizedTest
+        @ValueSource(ints = [0])
+        fun do_not_divide(zero: Int) {
+            assertThatThrownBy { calculator.divide(zero) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+
+            assertThrows<IllegalArgumentException> { calculator.divide(zero) }
+                .apply { assertThat(message).isEqualTo("Cannot divide by zero") }
+        }
     }
 }
