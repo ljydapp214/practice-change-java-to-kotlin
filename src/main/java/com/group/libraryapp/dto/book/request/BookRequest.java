2d3f541478548bd45bb86946c4c3f27a0fcf29cc
jaeyounglee <ljyda214@gmail.com>
2025-06-03 18:39:55 +0900

    feat: 클래스들에 주 생성자를 선언

--- src/main/java/com/group/libraryapp/dto/book/request/BookRequest.java
+++ src/main/java/com/group/libraryapp/dto/book/request/BookRequest.java
@@ -2,10 +2,19 @@ package com.group.libraryapp.dto.book.request;
 
 public class BookRequest {
 
-  private String name;
+    private String name;
+    private String type;
 
-  public String getName() {
-    return name;
-  }
+    public BookRequest(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
