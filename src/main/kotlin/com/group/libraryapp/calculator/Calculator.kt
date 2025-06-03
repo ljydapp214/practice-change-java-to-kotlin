3dcf5a4298bf4382a8b3c6654c9aa54398df3d1c
jaeyounglee <ljyda214@gmail.com>
2025-06-03 18:31:46 +0900

    chg: require() 을 통해 유효성을 검사하도록 변경

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -1,7 +1,7 @@
 package com.group.libraryapp.calculator
 
 data class Calculator(
-    private var number:Int
+    private var number: Int,
 ) {
     fun add(operand: Int) {
         this.number += operand
@@ -16,20 +16,17 @@ data class Calculator(
     }
 
     fun divide(operand: Int) {
-        if (operand == 0) {
-            throw IllegalArgumentException("Cannot divide by zero")
-        }
+        require(operand != 0) { "0으로는 수를 나눌 수 없습니다." }
         this.number /= operand
     }
 }
 
 // backing property 이용
 data class CalculatorV2(
-    private var _number:Int
+    private var _number: Int,
 ) {
     val number: Int
         get() = this._number
-
 
     fun add(operand: Int) {
         this._number += operand
