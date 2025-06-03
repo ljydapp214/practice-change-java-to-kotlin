f304ba0f406b9a231b809410e139e8ff5ff3ca6e
jaeyounglee <ljyda214@gmail.com>
2025-06-03 18:40:27 +0900

    feat: 신규 필드 type 을 선언

--- src/main/java/com/group/libraryapp/domain/book/Book.java
+++ src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -1,35 +1,40 @@
 package com.group.libraryapp.domain.book;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 @Entity
 public class Book {
 
-  @Id
-  @GeneratedValue(strategy = IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String name;
+    @Column(nullable = false)
+    private String name;
 
-  public Book() {
+    @Column(nullable = false)
+    private String type;
 
-  }
-
-  public Book(String name) {
-    if (name.isBlank()) {
-      throw new IllegalArgumentException("이름은 비어 있을 수 없습니다");
+    public Book() {
     }
-    this.name = name;
-  }
 
-  public String getName() {
-    return name;
-  }
+    public Book(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 비어 있을 수 없습니다");
+        }
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
