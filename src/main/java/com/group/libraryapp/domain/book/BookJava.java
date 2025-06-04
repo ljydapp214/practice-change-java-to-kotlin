f8031b60dd33e04c67892332b68a7dadaf2cf13e
jaeyounglee <ljyda214@gmail.com>
2025-06-04 23:50:26 +0900

    chg: 기존 java 의 Book 클래스 이름 변경

--- src/main/java/com/group/libraryapp/domain/book/BookJava.java
+++ src/main/java/com/group/libraryapp/domain/book/BookJava.java
@@ -1,14 +1,13 @@
 package com.group.libraryapp.domain.book;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
-@Entity
-public class Book {
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+//@Entity
+public class BookJava {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
@@ -20,10 +19,10 @@ public class Book {
     @Column(nullable = false)
     private String type;
 
-    public Book() {
+    public BookJava() {
     }
 
-    public Book(String name) {
+    public BookJava(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("이름은 비어 있을 수 없습니다");
         }
