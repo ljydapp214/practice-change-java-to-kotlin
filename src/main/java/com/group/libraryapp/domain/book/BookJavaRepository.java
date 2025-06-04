b929731582e3b59ae170acb7ebb37994476fa46c
jaeyounglee <ljyda214@gmail.com>
2025-06-04 23:51:16 +0900

    chg: BookRepository 를 kotlin 구현으로 변

---
+++ src/main/java/com/group/libraryapp/domain/book/BookJavaRepository.java
@@ -0,0 +1,7 @@
+package com.group.libraryapp.domain.book;
+
+//public interface BookJavaRepository extends JpaRepository<BookJava, Long> {
+//
+//  Optional<BookJava> findByName(String bookName);
+//
+//}
