20d7fe89e096d0f5f62b6be2adad5d0328cfc672
jaeyounglee <ljyda214@gmail.com>
2025-06-26 23:29:18 +0900

    chg: User 클래스 전환에 따른 변경사항 반영

--- src/main/java/com/group/libraryapp/domain/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/domain/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.group.libraryapp.domain.user;
 
+import com.group.libraryapp.domain.user.loanhistory.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -1,6 +1,5 @@
 package com.group.libraryapp.domain.user.loanhistory
 
-import com.group.libraryapp.domain.user.User
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -19,5 +18,12 @@ class UserLoanHistory(
 ) {
     fun doReturn() {
         this.isReturn = true
+    }
+
+    companion object {
+        fun fixture(
+            borrower: User,
+            bookName: String,
+        ): UserLoanHistory = UserLoanHistory(borrower, bookName, false, null)
     }
 }
