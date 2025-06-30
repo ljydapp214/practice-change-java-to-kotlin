3a31adf7a6775b2f1b7df942b012785af9bd2a3c
jaeyounglee <ljyda214@gmail.com>
2025-06-30 23:48:32 +0900

    rm: 자바 클래스 제거

--- src/main/java/com/group/libraryapp/domain/book/BookJavaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group.libraryapp.domain.book;
-
-//public interface BookJavaRepository extends JpaRepository<BookJava, Long> {
-//
-//  Optional<BookJava> findByName(String bookName);
-//
-//}
