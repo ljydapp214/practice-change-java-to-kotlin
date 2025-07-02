d9556684dfa8b1b0c2abd3c061736c8e808bf80f
jaeyounglee <ljyda214@gmail.com>
2025-07-03 01:29:42 +0900

    feat: 레포지토리를 자바에서 코틀린으로 변경

---
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -0,0 +1,10 @@
+package com.group.libraryapp.domain.user.loanhistory
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
+    fun findByBookNameAndIsReturn(
+        bookName: String,
+        isReturn: Boolean,
+    ): UserLoanHistory?
+}
