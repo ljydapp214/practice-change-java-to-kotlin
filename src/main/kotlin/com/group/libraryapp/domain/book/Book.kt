4a7eac6d07d6e4bab66ee52b00e051e72a9ca3f4
jaeyounglee <ljyda214@gmail.com>
2025-07-14 23:30:14 +0900

    feat: BookType 을 enum class 로 구현

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -2,6 +2,8 @@ package com.group.libraryapp.domain.book
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -13,8 +15,9 @@ import java.time.LocalDateTime
 class Book(
     @Column(name = "name", nullable = false)
     var name: String,
+    @Enumerated(value = EnumType.STRING)
     @Column(name = "type", nullable = false)
-    val type: String = "novel",
+    val type: BookType,
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 ) {
@@ -25,11 +28,17 @@ class Book(
         require(!name.isBlank()) { "이름은 비어있을 수 없습니다." }
     }
 
-    constructor(name: String) : this(name, "novel", null)
+    constructor(name: String) : this(name, BookType.COMPUTER, null)
 
-    constructor(name: String, type: String) : this(name, type, null)
+    constructor(name: String, type: String) : this(name, BookType.COMPUTER, null)
 
     companion object {
-        fun fixture(bookName: String): Book = Book(bookName)
+        fun fixture(bookName: String = "책 이름"): Book = Book(bookName)
+
+        fun fixture(
+            bookName: String = "책 이름",
+            type: BookType = BookType.COMPUTER,
+            id: Long?,
+        ): Book = Book(bookName, type, id)
     }
 }
---
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookType.kt
@@ -0,0 +1,9 @@
+package com.group.libraryapp.domain.book
+
+enum class BookType {
+    COMPUTER,
+    ECONOMICS,
+    SOCIETY,
+    LANGUAGE,
+    SCIENCE,
+}
